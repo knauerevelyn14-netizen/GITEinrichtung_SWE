78c7d8407d6a3b593ae3344568fa25ae049d73dc
naurin <naurinabubaker@gmail.com>
2026-04-05 12:46:09 +0200

    zweite klasse fur aufgabe 2

--- dozierender.java
+++ dozierender.java
@@ -1,1 +1,11 @@
-public class dozierender {}
+public class Dozierender {
+    
+    String name;         // The name of the lecturer 
+    String fakultaet;    // The faculty they belong to 
+    String buronummer;  // Their office number 
+
+    
+    public void frageBeantworten(Studierender student, String antwort) {
+        System.out.println("Hallo " + student.name + ", hier ist die Antwort: " + antwort);
+    }
+}
